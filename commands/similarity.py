83a671b09385d15d41ad95635590ac86d72cfa4c
Oliver Powell <mail@oliverpowell.com>
2023-02-02 13:23:11 +0000

    Print statement that update the user on the progress of similarity.py have been removed. Logging module is now used instead.

--- commands/similarity.py
+++ commands/similarity.py
@@ -1,15 +1,18 @@
 import csv
 import itertools
 import pickle
+import logging
 
 import click
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+
 
 def extract_mapping_data(sam_file):
     contig_read_dictionary = {}
-    print("extracting infromation from SAM file...")
+    logging.info("extracting infromation from SAM file...")
     with open(sam_file) as file:
         for line in file:
             if line.startswith("@SQ"):
@@ -32,16 +35,16 @@ def extract_mapping_data(sam_file):
 
 
 def convert_reads_to_hexidemical(contig_read_dictionary, index):
-    print("extracting infromation from index file...")
+    logging.info("extracting infromation from index file...")
     with open(index, mode='r') as inp:
         index_reader = csv.reader(inp)
         ID_colour_dict = {rows[0]: rows[1] for rows in index_reader}
 
-    print("converting Seq IDs to RGB values...")
+    logging.info("converting Seq IDs to RGB values...")
     contig_rgb = {contig: list(map(lambda x: ID_colour_dict[x], contig_read_dictionary[contig])) for contig in
                   contig_read_dictionary}
 
-    print("converting RGB values to hexidecimal values...")
+    logging.info("converting RGB values to hexidecimal values...")
     contig_hex_dictionary = {
         contig: list(map(lambda x: '#%02x%02x%02x' % tuple(map(int, x.split(","))), contig_rgb[contig])) for
         contig in contig_rgb}
@@ -50,25 +53,25 @@ def convert_reads_to_hexidemical(contig_read_dictionary, index):
 
 
 def calculate_cosine_similarity(contig_hexidemical_dictionary):
-    print('converting profiles to text...')
+    logging.info('converting profiles to text...')
     contig_adapter_profiles = [" ".join(contig_hexidemical_dictionary[contig]) for contig in
                                contig_hexidemical_dictionary]
 
     labels = contig_hexidemical_dictionary.keys()
 
-    print('vectorizing profiles...')
+    logging.info('vectorizing profiles...')
     count_array = CountVectorizer(max_df=0.2)
     profile_count_array = count_array.fit_transform(contig_adapter_profiles)
 
-    print('calculating cosine similarity...')
+    logging.info('calculating cosine similarity...')
     cosine_array = cosine_similarity(profile_count_array, dense_output=False)
 
-    print('converting sparse matrix to dictionary...')
+    logging.info('converting sparse matrix to dictionary...')
     cosine_dictionary = dict(cosine_array.todok())
 
     matched_contigs = {int(val): [] for val in range(1, len(labels) + 1)}
 
-    print('grouping contigs by cosine similarity...')
+    logging.info('grouping contigs by cosine similarity...')
     for i in cosine_dictionary:
         key = str(i)[1:-1].split(",")
         reference_contig = int(key[0]) + 1
@@ -78,13 +81,13 @@ def calculate_cosine_similarity(contig_hexidemical_dictionary):
         if cosine_value > 0.95:
             matched_contigs[reference_contig].append(matching_contig)
 
-    print('removing duplicate contig groups')
+    logging.info('removing duplicate contig groups')
     matched_contig_groups = list(matched_contigs.values())
     matched_contig_groups.sort()
     non_duplicate_contig_groups = list(
         matched_contig_groups for matched_contig_groups, _ in itertools.groupby(matched_contig_groups))
 
-    print('writing data to pickle...')
+    logging.info('writing data to pickle...')
     with open('grouped_contigs.pkl', 'wb') as f:
         pickle.dump(non_duplicate_contig_groups, f)
 
